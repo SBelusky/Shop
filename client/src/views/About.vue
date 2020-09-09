88df0a9eb5a98ae0610d75d4b5dc85f612a37ed3
Samuell <samuel.belusky@gmail.com>
2020-09-09 22:39:47 +0200

    v1.0.3
    - sfunkčnenie backendu+frontendu
    - doplnenie anotácie @CrossOrigin pre CustomerController.java

---
+++ client/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <tabley></tabley>
+  </div>
+</template>
+<script>
+import Tabley from "@/components/Table.vue";
+
+export default {
+  components: {
+    Tabley,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
